e74947aa75d61288be30e10f578d948543379dea
Federico Holc <federicoholc@gmail.com>
2024-11-24 09:33:47 -0300

    clean up Vitest configuration files

--- frontend/vitest.config.js
+++ frontend/vitest.config.js
@@ -5,8 +5,8 @@ dotenv.config({ path: ".env" });
 
 export default defineConfig({
   test: {
-    include: ["src/**/*.test.js"], // Incluir solo archivos .test.js en la carpeta src
-    exclude: ["src/**/*.spec.js"], // Excluir archivos .spec.js en la carpeta src
-    environment: "node", // Asegurarse de que el entorno de prueba sea Node.js
+    include: ["src/**/*.test.js"],
+    exclude: ["src/**/*.spec.js"],
+    environment: "node",
   },
 });
